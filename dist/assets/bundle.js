6ff279531aeb7b14528118f4dad52afb007030df
darren <darren__c@hotmail.com>
2020-03-11 14:01:35 +0900

    second commit babel is working

---
+++ dist/assets/bundle.js
@@ -0,0 +1,7 @@
+"use strict";
+
+var greeting = function greeting(name) {
+  console.log("Hi ".concat(name, " how are you doing?"));
+};
+
+greeting('frankie');
---
+++ src/index.js
@@ -0,0 +1,5 @@
+const greeting = (name) => {
+ console.log(`Hi ${name} how are you doing?`);
+}
+
+greeting('frankie');
